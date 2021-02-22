99233663f23fed0ddc68754622845509ce0c5f1f
Rufus Pollock <rufus.pollock@datopian.com>
2021-02-22 21:45:42 +0100

    [home][s]: stub f11s dataset and start displaying it.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 
-export default function Home() {
+export default function Home({dataset}) {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
@@ -10,9 +10,41 @@ export default function Home() {
 
       <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
-          XXX
+          { dataset.title }
         </h1>
+       <section>
+          {dataset.resources.map(({ id, date, title }) => (
+            <section key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </section>
+          ))}
+        </section>
       </main>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      dataset: {
+        title: 'My cool dataset',
+        resources: [
+          {
+            "id": "aaa",
+            "name": "xyz",
+            "date": "2012",
+            "sample": [
+              { "a": 1, "b": 2 },
+              { "a": 3, "b": 4 }
+            ]
+          }
+        ]
+      }
+    }
+  }
 }

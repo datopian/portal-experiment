e5ed818928ea0f1f259fab9ccf57f519f223ec96
Rufus Pollock <rufus.pollock@datopian.com>
2021-02-22 21:40:45 +0100

    [*][l]: create-next-app --with-template=tailwind.

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Head>
+        <title>Dataset</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
+        <h1 className="text-6xl font-bold">
+          XXX
+        </h1>
+      </main>
+    </div>
+  )
+}

44a319dcb4653ff506ee0b0c9d88f91a01bd5ae8
Rufus Pollock <rufus.pollock@datopian.com>
2021-02-22 22:38:24 +0100

    [showcase][m]: add a fixture dataset (finance-vix) and render it with new getDataset lib.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
+import path from 'path'
 
 export default function Home({dataset}) {
+  const descriptor = dataset.descriptor
+  const readme = dataset.readme
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
@@ -8,43 +11,34 @@ export default function Home({dataset}) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          { dataset.title }
+      <main className="font-mono flex flex-col items-center justify-center flex-1 px-20">
+        <h1 className="text-6xl font-bold text-center">
+          { descriptor.title }
         </h1>
        <section>
-          {dataset.resources.map(({ id, date, title }) => (
-            <section key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
+          {descriptor.resources.map((resource) => (
+            <section key={resource.name}>
+              {resource.name}
             </section>
           ))}
+        </section>
+        <section>
+          {readme}
         </section>
       </main>
     </div>
   )
 }
 
+const datasetsDirectory = path.join(process.cwd(), 'datasets')
+
+import { getDataset } from '../lib/dataset'
+
 export async function getStaticProps() {
+  const dataset = getDataset(datasetsDirectory)
   return {
     props: {
-      dataset: {
-        title: 'My cool dataset',
-        resources: [
-          {
-            "id": "aaa",
-            "name": "xyz",
-            "date": "2012",
-            "sample": [
-              { "a": 1, "b": 2 },
-              { "a": 3, "b": 4 }
-            ]
-          }
-        ]
-      }
+      dataset
     }
   }
 }

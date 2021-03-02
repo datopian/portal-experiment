a6278b50bf791f75bbc8816526850a2e6d7d7529
Rising Odegua <risingodegua@gmail.com>
2021-03-02 14:23:14 +0100

    [Test][s]: Add jest-dom to dev dependency

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
-    setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+    setupFilesAfterEnv: ["<rootDir>/tests/setupTests.js"],
+    setupFiles: ["<rootDir>/tests/setupTests.js"],
     transform: {
       "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
       "\\.(css|less|scss|sass)$": "identity-obj-proxy"
